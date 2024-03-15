83e18fcfaa970ecf79ba911f98b9c29c05cab73b
alinaios <85871814+alinaios@users.noreply.github.com>
2024-03-15 13:46:49 +0100

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by AH on 2024-03-15.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
